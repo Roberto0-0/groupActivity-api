7fc71641003de844aea7804d75c1637bea0dfc16
Robert <roberto88526@gmail.com>
2025-02-20 14:50:21 -0300

    code optimization based on the Group.js model

--- src/services/group/create.js
+++ src/services/group/create.js
@@ -2,10 +2,9 @@ const Group = require("../../models/Group")
 
 class GroupCreate {
     execute(groupProps) {
-        const { session, serialized, name, participants } = groupProps
+        const { session, serialized, name, amountParticipants } = groupProps
 
-        var newGroup = new Group(session, serialized, name, participants)
-        newGroup.amountParticipants = newGroup.getNumberOfParticipants(participants)
+        var newGroup = new Group(session, serialized, name, amountParticipants)
 
         return {
             success: true,
